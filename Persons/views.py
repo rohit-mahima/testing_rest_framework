1799786034b7146bd9fc6cc3e1035ce4adc607b6
rranja33 <rohitranjanmahima@gmail.com>
2022-07-08 14:10:42 +0530

    testing the rest framework

---
+++ Persons/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User,Group
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all().order_by('-date_joined')
+    serializer_class=UserSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset=Group.objects.all()
+    serializer_class=GroupSerializer
+    permission_classes=[permissions.IsAuthenticated]
